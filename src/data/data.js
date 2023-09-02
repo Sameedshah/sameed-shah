765af2765bcfcbe55bf0a5381e36260496a2eddc
SameedShah <sameedshahdev@gmail.com>
2023-09-02 23:37:16 +0500

    alpha

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/8cRGtszy/image.png',
+    title: "Todo List App",
+    desc: "The Todo List App Project using React Js", //description
+    link: "https://todo-app-lac-gamma.vercel.app",
+    git: "https://github.com/Sameedshah/todo-app.git",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/ry723B01/image.png',
+    desc: "Personal Portfolio Website Using Next Js, Tailwind CSS and Typescript",
+    title: "Personal Portfolio",
+    link: "https://sameedshah-portfolio.vercel.app",
+    git: "https://github.com/Sameedshah/sameedshah-portfolio.git",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/SsTknjx3/image.png',
+    title: "WeBuy Shopping App",
+    desc: "WeBuy Shopping App Using Next Js, Tailwind CSS and Typescript",
+    link: "",
+    git: "",
+  }
+  
+];
